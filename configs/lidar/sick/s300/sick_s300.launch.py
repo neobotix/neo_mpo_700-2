94dab689f93d8fabf43634ef0c7e351ee386dc0e
neobotix <padhupradheep@gmail.com>
2021-06-30 16:36:58 +0000

    Params tested and updated

--- configs/lidar/sick/s300/sick_s300.launch.py
+++ configs/lidar/sick/s300/sick_s300.launch.py
@@ -9,10 +9,10 @@ from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
 
-    config = os.path.join(get_package_share_directory('neo_mpo_500-2'),'configs/lidar/sick/s300','s300_1.yaml')
-    config1 = os.path.join(get_package_share_directory('neo_mpo_500-2'),'configs/lidar/sick/s300','s300_filter_1.yaml')
-    config2 = os.path.join(get_package_share_directory('neo_mpo_500-2'),'configs/lidar/sick/s300','s300_2.yaml')
-    config3 = os.path.join(get_package_share_directory('neo_mpo_500-2'),'configs/lidar/sick/s300','s300_filter_2.yaml')
+    config = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/lidar/sick/s300','s300_1.yaml')
+    config1 = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/lidar/sick/s300','s300_filter_1.yaml')
+    config2 = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/lidar/sick/s300','s300_2.yaml')
+    config3 = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/lidar/sick/s300','s300_filter_2.yaml')
 
 
     return launch.LaunchDescription([
