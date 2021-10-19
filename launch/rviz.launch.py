e2fe78da0150ad096cea9e23a02951459e445565
Pradheep-office <padhupradheep@gmail.com>
2021-10-19 17:11:56 +0200

    adding rviz

---
+++ launch/rviz.launch.py
@@ -0,0 +1,27 @@
+# Neobotix GmbH
+# Author: Pradheep Padmanabhan
+
+import launch
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir, LaunchConfiguration
+from launch_ros.actions import Node
+import os
+from pathlib import Path
+
+def generate_launch_description():
+    rviz_config_dir = os.path.join(
+        get_package_share_directory('nav2_bringup'),
+        'rviz',
+        'nav2_default_view.rviz')
+
+    rviz =   Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            output='screen')
+
+    return LaunchDescription([rviz])
