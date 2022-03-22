a599e6d0d3e44593589102f98aa03c10b3e3509e
Pradheep Krishna <padhupradheep@gmail.com>
2022-03-22 15:34:23 +0100

    handling namespace
    * adding namespaces to the kinematics node
    
    * handling namespaces for lidar
    
    * updating relayboard params
    
    * adding namespaces for teleop
    
    * adding the updated bringup
    
    * handling single relay scan topics for namespaces
    
    * fixing launch for laser scanners
    
    * frame prefix added with a clean up of launch
    
    * added a standalone map_server and adapting namespace with a master robot
    
    * launch fixes
    
    * updating launch
    
    * updating params
    
    * topic tools fix

--- configs/teleop/teleop.launch.py
+++ configs/teleop/teleop.launch.py
@@ -6,13 +6,23 @@ from ament_index_python.packages import get_package_share_directory
 import launch_ros.actions
 
 def generate_launch_description():
+    robot_namespace = launch.substitutions.LaunchConfiguration('namespace', default="")
+
     config = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/teleop','teleop.yaml')
     return launch.LaunchDescription([
         launch_ros.actions.Node(
-            package='neo_teleop2', executable='neo_teleop2_node', output='screen',
-            name='neo_teleop2_node', parameters = [config])
+            package='neo_teleop2',
+            executable='neo_teleop2_node',
+            output='screen',
+            name='neo_teleop2_node',
+            namespace=robot_namespace,
+            parameters = [config])
     ,
      launch_ros.actions.Node(
-            package='joy', executable='joy_node', output='screen',
-            name='joy_node', parameters = [{'dev': "/dev/input/js0"}, {'deadzone':0.12}])
+            package='joy', 
+            executable='joy_node', 
+            output='screen',
+            name='joy_node',
+            namespace=robot_namespace,
+            parameters = [{'dev': "/dev/input/js0"}, {'deadzone':0.12}])
     ])
