40b50fa8957a6499c54b0a75cf200e199b20f1d5
Pradheep Krishna <padhupradheep@gmail.com>
2023-02-01 15:12:43 +0100

    Feature/phidgets imu (#22)
    * updating launch to support IMU
    
    * making IMU optional + pointing the drivers to the frames
    
    * condition to enable IMU
    
    * adding dependency for phidgets

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -10,16 +10,22 @@ from launch_ros.actions import Node
 import os
 from pathlib import Path
 from launch.launch_context import LaunchContext
+from launch.conditions import IfCondition
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
     neo_mpo_700 = get_package_share_directory('neo_mpo_700-2')
     robot_namespace = LaunchConfiguration('robot_namespace', default='')
+    imu_enable = LaunchConfiguration('imu_enable', default='False')
     context = LaunchContext()
 
     urdf = os.path.join(get_package_share_directory('neo_mpo_700-2'), 'robot_model/mpo_700', 'mpo_700.urdf')
 
     with open(urdf, 'r') as infp:  
         robot_desc = infp.read()
+
+    phidgets_drivers_file_dir = os.path.join(get_package_share_directory('phidgets_spatial'), 'launch')
 
     start_robot_state_publisher_cmd = Node(
         package='robot_state_publisher',
@@ -29,6 +35,23 @@ def generate_launch_description():
         namespace=robot_namespace,
         parameters=[{'robot_description': robot_desc, 'frame_prefix': robot_namespace}],
 		arguments=[urdf])
+
+    # Note: imu_link needs to be manually uncommented in the URDF
+    start_imu_driver = ComposableNodeContainer(
+            name='phidget_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='phidgets_spatial',
+                    plugin='phidgets::SpatialRosI',
+                    name='phidgets_spatial'),
+            ],
+            output='both',
+            condition=IfCondition(imu_enable),
+            parameters=[{'frame_id': robot_namespace.perform(context) + "imu_link"}],
+    )
 	
 	# Launch can be set just once, does not matter if you set it for other launch files. 
 	# The arguments should certainly have different meaning if there is a bigger launch file
@@ -78,4 +101,11 @@ def generate_launch_description():
             output='screen',
             parameters=[{'input_topic': robot_namespace.perform(context) + "lidar_2/scan_filtered",'output_topic': robot_namespace.perform(context) + "scan"}])
 
-    return LaunchDescription([relayboard, start_robot_state_publisher_cmd, laser, kinematics, teleop, relay_topic_lidar1, relay_topic_lidar2])
+    return LaunchDescription([relayboard,
+        start_imu_driver,
+        start_robot_state_publisher_cmd,
+        laser,
+        kinematics,
+        teleop,
+        relay_topic_lidar1,
+        relay_topic_lidar2])
