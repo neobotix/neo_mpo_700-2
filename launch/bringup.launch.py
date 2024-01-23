d9835008004146c09aaa9e1a93d959b2847ae9e3
PRP <padhupradheep@gmail.com>
2024-01-23 08:54:37 +0100

    RViz for UR is not needed

--- launch/bringup.launch.py
+++ launch/bringup.launch.py
@@ -107,6 +107,7 @@ def execution_stage(context: LaunchContext,
                 launch_arguments={
                     'ur_type': arm_typ,
                     'robot_ip': "192.168.0.101",
+                    'launch_rviz': False
                 }.items()
             )
 
