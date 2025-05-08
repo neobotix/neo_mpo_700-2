528fa9e07f27e23991fab5e437a5380b31575168
Pradheep Krishna <padhupradheep@gmail.com>
2025-05-08 13:49:40 +0200

    Gz sim, gripper and lot more (#54)
    * Completed migration to modern gazebo, reafactore launch files, modularized and restructured urdf, and tested moveit for ur
    
    * Added 2f_140 robotiq gripper support
    
    * fixes and chore
    
    * removing the gripper controller in ur_control, keeping the gripper separate
    
    * fixes the visualization
    
    * Added seperate controller manager for 2f_140 robotiq gripper (#53)
    
    * Added seperate controller managers for gripper and arm, removed gripper from moveit, tested changes
    
    * Fixed ros2_control issues with 2f_140 gripper, support for 2f_85 gripper
    
    * changed  to  for consistency
    
    * Removed deprecated gripper_simulation_controllers arg from urdf
    
    * Minor formatting fixes and removed unused launch arg (#57)
    
    ---------
    
    Co-authored-by: Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
    Co-authored-by: Adarsh Karan Kesavadas Prasanth <112091438+AdarshKaran@users.noreply.github.com>

--- configs/ur/ur_control.launch.py
+++ configs/ur/ur_control.launch.py
@@ -29,226 +29,60 @@
 #
 # Author: Denis Stogl
 
-from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterFile, ParameterValue
-from launch_ros.substitutions import FindPackageShare
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import AnyLaunchDescriptionSource
 from launch.substitutions import (
     AndSubstitution,
-    Command,
-    FindExecutable,
     LaunchConfiguration,
     NotSubstitution,
     PathJoinSubstitution,
 )
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterFile
+from launch_ros.substitutions import FindPackageShare
 
 
-def launch_setup(context, *args, **kwargs):
+def launch_setup(context):
     # Initialize Arguments
     ur_type = LaunchConfiguration("ur_type")
     robot_ip = LaunchConfiguration("robot_ip")
-    safety_limits = LaunchConfiguration("safety_limits")
-    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
-    safety_k_position = LaunchConfiguration("safety_k_position")
     # General arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
     controllers_file = LaunchConfiguration("controllers_file")
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
-    kinematics_params_file = LaunchConfiguration("kinematics_params_file")
-    tf_prefix = LaunchConfiguration("tf_prefix")
-    use_fake_hardware = LaunchConfiguration("use_fake_hardware")
-    fake_sensor_commands = LaunchConfiguration("fake_sensor_commands")
+    description_launchfile = LaunchConfiguration("description_launchfile")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
     controller_spawner_timeout = LaunchConfiguration("controller_spawner_timeout")
     initial_joint_controller = LaunchConfiguration("initial_joint_controller")
     activate_joint_controller = LaunchConfiguration("activate_joint_controller")
     launch_rviz = LaunchConfiguration("launch_rviz")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
     headless_mode = LaunchConfiguration("headless_mode")
     launch_dashboard_client = LaunchConfiguration("launch_dashboard_client")
     use_tool_communication = LaunchConfiguration("use_tool_communication")
-    tool_parity = LaunchConfiguration("tool_parity")
-    tool_baud_rate = LaunchConfiguration("tool_baud_rate")
-    tool_stop_bits = LaunchConfiguration("tool_stop_bits")
-    tool_rx_idle_chars = LaunchConfiguration("tool_rx_idle_chars")
-    tool_tx_idle_chars = LaunchConfiguration("tool_tx_idle_chars")
     tool_device_name = LaunchConfiguration("tool_device_name")
     tool_tcp_port = LaunchConfiguration("tool_tcp_port")
-    tool_voltage = LaunchConfiguration("tool_voltage")
-    reverse_ip = LaunchConfiguration("reverse_ip")
-    script_command_port = LaunchConfiguration("script_command_port")
-    reverse_port = LaunchConfiguration("reverse_port")
-    script_sender_port = LaunchConfiguration("script_sender_port")
-    trajectory_port = LaunchConfiguration("trajectory_port")
-
-    joint_limit_params = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", ur_type, "joint_limits.yaml"]
-    )
-    physical_params = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", ur_type, "physical_parameters.yaml"]
-    )
-    visual_params = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", ur_type, "visual_parameters.yaml"]
-    )
-    script_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_client_library"), "resources", "external_control.urscript"]
-    )
-    input_recipe_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_robot_driver"), "resources", "rtde_input_recipe.txt"]
-    )
-    output_recipe_filename = PathJoinSubstitution(
-        [FindPackageShare("ur_robot_driver"), "resources", "rtde_output_recipe.txt"]
-    )
-
-    robot_description_content = Command(
-        [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
-            " ",
-            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
-            " ",
-            "robot_ip:=",
-            robot_ip,
-            " ",
-            "joint_limit_params:=",
-            joint_limit_params,
-            " ",
-            "kinematics_params:=",
-            kinematics_params_file,
-            " ",
-            "physical_params:=",
-            physical_params,
-            " ",
-            "visual_params:=",
-            visual_params,
-            " ",
-            "safety_limits:=",
-            safety_limits,
-            " ",
-            "safety_pos_margin:=",
-            safety_pos_margin,
-            " ",
-            "safety_k_position:=",
-            safety_k_position,
-            " ",
-            "name:=",
-            ur_type,
-            " ",
-            "script_filename:=",
-            script_filename,
-            " ",
-            "input_recipe_filename:=",
-            input_recipe_filename,
-            " ",
-            "output_recipe_filename:=",
-            output_recipe_filename,
-            " ",
-            "tf_prefix:=",
-            tf_prefix,
-            " ",
-            "use_fake_hardware:=",
-            use_fake_hardware,
-            " ",
-            "fake_sensor_commands:=",
-            fake_sensor_commands,
-            " ",
-            "headless_mode:=",
-            headless_mode,
-            " ",
-            "use_tool_communication:=",
-            use_tool_communication,
-            " ",
-            "tool_parity:=",
-            tool_parity,
-            " ",
-            "tool_baud_rate:=",
-            tool_baud_rate,
-            " ",
-            "tool_stop_bits:=",
-            tool_stop_bits,
-            " ",
-            "tool_rx_idle_chars:=",
-            tool_rx_idle_chars,
-            " ",
-            "tool_tx_idle_chars:=",
-            tool_tx_idle_chars,
-            " ",
-            "tool_device_name:=",
-            tool_device_name,
-            " ",
-            "tool_tcp_port:=",
-            tool_tcp_port,
-            " ",
-            "tool_voltage:=",
-            tool_voltage,
-            " ",
-            "reverse_ip:=",
-            reverse_ip,
-            " ",
-            "script_command_port:=",
-            script_command_port,
-            " ",
-            "reverse_port:=",
-            reverse_port,
-            " ",
-            "script_sender_port:=",
-            script_sender_port,
-            " ",
-            "trajectory_port:=",
-            trajectory_port,
-            " ",
-        ]
-    )
-    robot_description = {
-        "robot_description": ParameterValue(value=robot_description_content, value_type=str)
-    }
-
-    initial_joint_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
-    )
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
-    )
-
-    # define update rate
-    update_rate_config_file = PathJoinSubstitution(
-        [
-            FindPackageShare(runtime_config_package),
-            "config",
-            ur_type.perform(context) + "_update_rate.yaml",
-        ]
-    )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[
-            robot_description,
-            update_rate_config_file,
-            ParameterFile(initial_joint_controllers, allow_substs=True),
+            LaunchConfiguration("update_rate_config_file"),
+            ParameterFile(controllers_file, allow_substs=True),
+            # We use the tf_prefix as substitution in there, so that's why we keep it as an
+            # argument for this launchfile
         ],
         output="screen",
-        condition=IfCondition(use_fake_hardware),
-    )
-
-    ur_control_node = Node(
-        package="ur_robot_driver",
-        executable="ur_ros2_control_node",
-        parameters=[
-            robot_description,
-            update_rate_config_file,
-            ParameterFile(initial_joint_controllers, allow_substs=True),
-        ],
-        output="screen",
-        condition=UnlessCondition(use_fake_hardware),
     )
 
     dashboard_client_node = Node(
         package="ur_robot_driver",
         condition=IfCondition(
-            AndSubstitution(launch_dashboard_client, NotSubstitution(use_fake_hardware))
+            AndSubstitution(launch_dashboard_client, NotSubstitution(use_mock_hardware))
         ),
         executable="dashboard_client",
         name="dashboard_client",
@@ -277,6 +111,7 @@ def launch_setup(context, *args, **kwargs):
         executable="urscript_interface",
         parameters=[{"robot_ip": robot_ip}],
         output="screen",
+        condition=UnlessCondition(use_mock_hardware),
     )
 
     controller_stopper_node = Node(
@@ -285,7 +120,7 @@ def launch_setup(context, *args, **kwargs):
         name="controller_stopper",
         output="screen",
         emulate_tty=True,
-        condition=UnlessCondition(use_fake_hardware),
+        condition=UnlessCondition(use_mock_hardware),
         parameters=[
             {"headless_mode": headless_mode},
             {"joint_controller_active": activate_joint_controller},
@@ -295,27 +130,10 @@ def launch_setup(context, *args, **kwargs):
                     "force_torque_sensor_broadcaster",
                     "joint_state_broadcaster",
                     "speed_scaling_state_broadcaster",
-                    "tcp_pose_broadcaster",
                     "ur_configuration_controller",
                 ]
             },
         ],
-    )
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="both",
-        parameters=[robot_description],
-    )
-
-    rviz_node = Node(
-        package="rviz2",
-        condition=IfCondition(launch_rviz),
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
     )
 
     # Spawn controllers
@@ -351,9 +169,6 @@ def launch_setup(context, *args, **kwargs):
         controllers_active.append(initial_joint_controller.perform(context))
         controllers_inactive.remove(initial_joint_controller.perform(context))
 
-    if use_fake_hardware.perform(context) == "true":
-        controllers_active.remove("tcp_pose_broadcaster")
-
     controller_spawners = [
         controller_spawner(controllers_active),
         controller_spawner(controllers_inactive, active=False),
@@ -361,13 +176,10 @@ def launch_setup(context, *args, **kwargs):
 
     nodes_to_start = [
         control_node,
-        ur_control_node,
         dashboard_client_node,
         tool_communication_node,
         controller_stopper_node,
         urscript_interface,
-        # robot_state_publisher_node,
-        # rviz_node,
     ] + controller_spawners
 
     return nodes_to_start
@@ -380,7 +192,17 @@ def generate_launch_description():
         DeclareLaunchArgument(
             "ur_type",
             description="Type/series of used UR robot.",
-            choices=["ur3", "ur3e", "ur5", "ur5e", "ur10", "ur10e", "ur16e", "ur20", "ur30"],
+            choices=[
+                "ur3",
+                "ur3e",
+                "ur5",
+                "ur5e",
+                "ur10",
+                "ur10e",
+                "ur16e",
+                "ur20",
+                "ur30",
+            ],
         )
     )
     declared_arguments.append(
@@ -412,46 +234,22 @@ def generate_launch_description():
     # General arguments
     declared_arguments.append(
         DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="ur_robot_driver",
-            description='Package with the controller\'s configuration in "config" folder. '
-            "Usually the argument is not set, it enables use of a custom setup.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "controllers_file",
-            default_value="ur_controllers.yaml",
+            default_value=PathJoinSubstitution(
+                [FindPackageShare("ur_robot_driver"), "config", "ur_controllers.yaml"]
+            ),
             description="YAML file with the controllers configuration.",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "description_package",
-            default_value="ur_description",
-            description="Description package with robot URDF/XACRO files. Usually the argument "
-            "is not set, it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="ur.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "kinematics_params_file",
+            "description_launchfile",
             default_value=PathJoinSubstitution(
-                [
-                    FindPackageShare(LaunchConfiguration("description_package")),
-                    "config",
-                    LaunchConfiguration("ur_type"),
-                    "default_kinematics.yaml",
-                ]
+                [FindPackageShare("ur_robot_driver"), "launch", "ur_rsp.launch.py"]
             ),
-            description="The calibration configuration of the actual robot used.",
+            description="Launchfile (absolute path) providing the description. "
+            "The launchfile has to start a robot_state_publisher node that "
+            "publishes the description topic.",
         )
     )
     declared_arguments.append(
@@ -465,17 +263,17 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "use_fake_hardware",
+            "use_mock_hardware",
             default_value="false",
-            description="Start robot with fake hardware mirroring command to its states.",
+            description="Start robot with mock hardware mirroring command to its states.",
         )
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "fake_sensor_commands",
+            "mock_sensor_commands",
             default_value="false",
-            description="Enable fake command interfaces for sensors used for simple simulations. "
-            "Used only if 'use_fake_hardware' parameter is true.",
+            description="Enable mock command interfaces for sensors used for simple simulations. "
+            "Used only if 'use_mock_hardware' parameter is true.",
         )
     )
     declared_arguments.append(
@@ -488,7 +286,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "controller_spawner_timeout",
-            default_value="10",
+            default_value="300",
             description="Timeout used when spawning controllers.",
         )
     )
@@ -501,8 +299,6 @@ def generate_launch_description():
                 "joint_trajectory_controller",
                 "forward_velocity_controller",
                 "forward_position_controller",
-                "freedrive_mode_controller",
-                "passthrough_trajectory_controller",
             ],
             description="Initially loaded robot controller.",
         )
@@ -519,7 +315,18 @@ def generate_launch_description():
     )
     declared_arguments.append(
         DeclareLaunchArgument(
-            "launch_dashboard_client", default_value="true", description="Launch Dashboard Client?"
+            "rviz_config_file",
+            default_value=PathJoinSubstitution(
+                [FindPackageShare("ur_description"), "rviz", "view_robot.rviz"]
+            ),
+            description="RViz config file (absolute path) to use when launching rviz.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "launch_dashboard_client",
+            default_value="true",
+            description="Launch Dashboard Client?",
         )
     )
     declared_arguments.append(
@@ -626,6 +433,22 @@ def generate_launch_description():
             "trajectory_port",
             default_value="50003",
             description="Port that will be opened for trajectory control.",
+        )
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            name="update_rate_config_file",
+            default_value=[
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("ur_robot_driver"),
+                        "config",
+                    ]
+                ),
+                "/",
+                LaunchConfiguration("ur_type"),
+                "_update_rate.yaml",
+            ],
         )
     )
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -1,42 +1,42 @@
 # Neobotix GmbH
 # Author: Pradheep Padmanabhan
+# Contributor: Adarsh Karan K P
 
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.actions import (
+  DeclareLaunchArgument,
+  IncludeLaunchDescription,
+  GroupAction,
+  OpaqueFunction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node, PushRosNamespace
 from launch.conditions import IfCondition
+from launch.launch_context import LaunchContext
 
 
-def generate_launch_description():
-    use_multi_robots = LaunchConfiguration('use_multi_robots', default='False')
-    head_robot = LaunchConfiguration('head_robot', default='False')
-    use_amcl = LaunchConfiguration('use_amcl', default='False')
-    use_sim_time = LaunchConfiguration('use_sim_time', default='False')
-    autostart = LaunchConfiguration('autostart', default='true')
-    namespace = LaunchConfiguration('namespace', default='')
-    map_dir = LaunchConfiguration(
-        'map',
-        default=os.path.join(
-            get_package_share_directory('neo_mpo_700-2'),
-            'configs/navigation/maps',
-            'test1.yaml'))
+def execution_stage(
+        context: LaunchContext,
+        use_sim_time,
+        autostart, namespace, use_multi_robots,
+        head_robot, use_amcl, map_dir, 
+        param_dir, use_rviz):
 
-    param_file_name = 'navigation.yaml'
-    param_dir = LaunchConfiguration(
-        'params_file',
-        default=os.path.join(
-            get_package_share_directory('neo_mpo_700-2'),
-            'configs/navigation',
-            param_file_name))
+    launch_actions = []
+
+    params = str(param_dir.perform(context))
+
+    # If the parameter file is not provided, use the default one based on the robot type
+    if not params:
+        params = os.path.join(
+                get_package_share_directory('neo_mpo_700-2'),
+                'configs', 'navigation',
+                'navigation.yaml')
 
     nav2_launch_file_dir = os.path.join(get_package_share_directory('neo_nav2_bringup'), 'launch')
-
-    ld = LaunchDescription()
 
     # Start navigation and push namespace if and only if the multi robot scenario is set to true. 
     start_navigation = GroupAction([
@@ -50,9 +50,10 @@ def generate_launch_description():
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
                 'use_multi_robots': use_multi_robots,
-                'params_file': param_dir,
+                'params_file': params,
                 'namespace': namespace}.items(),
         ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/localization_amcl.launch.py']),
             condition=IfCondition(use_amcl),
@@ -60,15 +61,18 @@ def generate_launch_description():
                 'map': map_dir,
                 'use_sim_time': use_sim_time,
                 'use_multi_robots': use_multi_robots,
-                'params_file': param_dir,
+                'params_file': params,
                 'namespace': namespace}.items(),
         ),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([nav2_launch_file_dir, '/navigation_neo.launch.py']),
-            launch_arguments={'namespace': namespace,
-                              'use_sim_time': use_sim_time,
-                              'params_file': param_dir}.items()),
+            launch_arguments={
+                'namespace': namespace,
+                'use_sim_time': use_sim_time,
+                'params_file': params,
+                'use_rviz': use_rviz}.items()
+        )
     ])
 
     # Start map_server if this robot is assigned as the head robot and if there is no multi-robot,
@@ -91,12 +95,96 @@ def generate_launch_description():
             name='lifecycle_manager_localization',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time},
-						{'autostart': autostart},
-                        {'node_names': ['map_server']}])
+                        {'autostart': autostart},
+                        {'node_names': ['map_server']}]
+            )
         ]
     )
 
-    ld.add_action(start_navigation)
-    ld.add_action(start_map_server)
+    launch_actions.append(start_navigation)
+    launch_actions.append(start_map_server)
 
-    return ld
+    return launch_actions
+
+def generate_launch_description():
+    launch_desc = LaunchDescription()
+    use_multi_robots = LaunchConfiguration('use_multi_robots')
+    head_robot = LaunchConfiguration('head_robot')
+    use_amcl = LaunchConfiguration('use_amcl')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    autostart = LaunchConfiguration('autostart')
+    namespace = LaunchConfiguration('robot_namespace')
+    map_dir = LaunchConfiguration('map')
+    param_dir = LaunchConfiguration('nav2_params_file')
+    use_rviz = LaunchConfiguration('use_rviz')
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+            'use_sim_time', default_value='false',
+            description='Use simulation clock if true'
+        )
+    
+    declare_autostart_cmd = DeclareLaunchArgument(
+            'autostart', default_value='true',
+            description='Automatically start the nav2 stack'
+        )
+    
+    declare_namespace_cmd = DeclareLaunchArgument(
+            'robot_namespace', default_value='',
+            description='Top-level namespace'
+        )
+    
+    declare_use_multi_robots_cmd = DeclareLaunchArgument(
+            'use_multi_robots', default_value='False',
+            description='Use multi robots'
+        )
+    
+    declare_head_robot_cmd = DeclareLaunchArgument(
+            'head_robot', default_value='False',
+            description='Head robot'
+        )
+    
+    declare_use_amcl_cmd = DeclareLaunchArgument(
+            'use_amcl', default_value='False',
+            description='Use AMCL'
+        )
+    
+    declare_map_cmd = DeclareLaunchArgument(
+            'map', default_value=os.path.join(
+                get_package_share_directory('neo_mpo_700-2'),
+                'configs',
+                'navigation',
+                'maps',
+                'neo_workshop.yaml'),
+            description='Full path to map file to load'
+        )
+    
+    declare_nav2_param_file_cmd = DeclareLaunchArgument(
+            'nav2_params_file', default_value="",
+            description='Full path to the Nav2 parameters file to load.\n'
+                        '\tLeave empty to use the default file based on the robot type'
+        )
+    
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+            'use_rviz', default_value='True',
+            description='Launch RViz for visualization'
+        )
+    
+    # Adding all the necessary launch description actions
+    launch_desc.add_action(declare_use_sim_time_cmd)
+    launch_desc.add_action(declare_autostart_cmd)
+    launch_desc.add_action(declare_namespace_cmd)
+    launch_desc.add_action(declare_use_multi_robots_cmd)
+    launch_desc.add_action(declare_head_robot_cmd)
+    launch_desc.add_action(declare_use_amcl_cmd)
+    launch_desc.add_action(declare_map_cmd)
+    launch_desc.add_action(declare_nav2_param_file_cmd)
+    launch_desc.add_action(declare_use_rviz_cmd)
+
+    context_arguments = [use_sim_time, autostart, namespace,
+                         use_multi_robots, head_robot, use_amcl, map_dir, param_dir, use_rviz]
+
+    opq_function = OpaqueFunction(function=execution_stage, args=context_arguments)
+
+    launch_desc.add_action(opq_function)
+
+    return launch_desc
