96c21ba0661467a2e2bdfc17edc17a8bcadbf5fd
Adarsh Karan Kesavadas Prasanth <adarshkaran01@gmail.com>
2025-06-10 16:46:03 +0530

    Added author names

--- launch/mapping.launch.py
+++ launch/mapping.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Open Source Robotics Foundation, Inc.
+# Copyright 2025 Neobotix GmbH.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Author: Darby Lim
+# Authors: Pradheep Padmanabhan, Adarsh Karan K P
+# Co-Author: Darby Lim
 
 import os
 
